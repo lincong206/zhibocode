20e55b4b1da075fcbd74bd709acddfbdef3269f4
lincong <408905030@qq.com>
2022-12-04 19:45:22 +0800

    push project

---
+++ zhiboTV/zhiboTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zhiboTV
+//
+//  Created by 林聪 on 2022/12/4.
+//  Copyright © 2022 Lynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ zhiboTV/zhiboTVTests/zhiboTVTests.swift
@@ -0,0 +1,34 @@
+//
+//  zhiboTVTests.swift
+//  zhiboTVTests
+//
+//  Created by 林聪 on 2022/12/4.
+//  Copyright © 2022 Lynn. All rights reserved.
+//
+
+import XCTest
+@testable import zhiboTV
+
+class zhiboTVTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
